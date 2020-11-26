f6f60a97f0d44a4f10f67e8522f5a00b72683028
bowen125125 <bowen125125@gmail.com>
2020-11-26 09:29:47 +0800

    final

--- index.js
+++ index.js
@@ -31,34 +31,38 @@ const bot = linebot({
 bot.on('message', async event => {
   try{
     let texts = event.message.text
-    let reply = {}
+    let reply ={}
     for(const b in bikeLocations) {
       if(bikeLocations[b].sna.includes(texts)){
-        reply = {
+       
+        let results = {
           type: 'template',
           altText: 'this is a buttons template',
           template: {
             type: 'buttons',
             thumbnailImageUrl: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/455bcb41754207.57b32e52eda95.jpg',
             title: bikeLocations[b].sna,
-            text: bikeLocations[b].ar,
-            actions: [{
+            text: `${bikeLocations[b].snaen}\n可借數量 :${bikeLocations[b].sbi} \n可還數量 :${bikeLocations[b].bemp}`,
+            actions: [ {
               type: 'message',
-              label: '可借數量',
-              text: bikeLocations[b].sna + '可借數量'
-            }, {
-              type: 'message',
-              label: '可還數量',
-              text: bikeLocations[b].sna + '可還數量'
+              label: '看詳細地址',
+              text: bikeLocations[b].sna + '詳細地址'
             },
             {
               type: 'message',
-              label: '位置地圖',
+              label: '看位置地圖',
               text: bikeLocations[b].sna + '位置地圖'
             }]
           }
         }
-        break
+        reply = results
+        
+      }
+      if(texts === bikeLocations[b].sna + '詳細地址'){
+        reply={
+          type: 'text', 
+          text: bikeLocations[b].ar
+        }
       }
       if(texts === bikeLocations[b].sna + '位置地圖'  ){
         reply={
@@ -72,12 +76,11 @@ bot.on('message', async event => {
       // 錯誤代碼查詢
       if( texts === '錯誤代碼'){
         reply={
-          type: 'text',
-          text: '錯誤代碼[8]'
+          type: "image",
+          originalContentUrl: "https://i.imgur.com/o9RzK4g.png",
+          previewImageUrl: "https://i.imgur.com/o9RzK4g.png"
         }
-      }
-      
-      
+      }      
     }
     // 不符合時回傳
     reply = (reply.length === 0) ? '沒有資料' : reply
